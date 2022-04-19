c7b77cc4ba57a219bb508183ddf4cfbca8461bb3
Ethan Connelly <connellyethan@gmail.com>
2022-04-19 12:48:55 -0400

    made responsive

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -15,7 +15,7 @@ const Main=()=>{
         <>
         <div className="header">
             <div className="row1">
-                <h1>A rooom without books is like <br /> a body without a soul. </h1>
+                <h1>A room without books is like <br /> a body without a soul. </h1>
             </div>
             <div className="row2">
                 <h2>Find Your Book</h2>
